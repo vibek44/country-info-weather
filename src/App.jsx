6990cdef67a2c75691cfad977576442ea4f0d521
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-11-24 10:49:57 +0200

    Adding useEffect to fetch weather data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import SearchList from './components/SearchList.jsx'
+import CountryInfo from './components/CountryInfo.jsx'
 
-function App() {
-  //const [countries, setCount] = useState(null)
 
-  return (
-    <>
-     <p>Weather info app</p>
- 
-    </>
+const api_key=import.meta.env.VITE_SOME_KEY
+
+
+const App=()=> {
+  const [countries, setCountries] = useState([])
+  const[weather,setWeather]=useState({})
+  const [search, setSearch]=useState('')
+  
+
+  const countriesToShow=(countries.filter(country=>
+      country.name.common.toLowerCase().includes(search.toLowerCase()))
+    )
+   
+   let capital=countriesToShow.length===1 ? countriesToShow[0].capital[0]:undefined
+    
+  useEffect(()=>{
+     console.log('country')
+      axios.get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+        .then(res=>{
+         
+          setCountries(res.data)
+        })
+  },[capital])
+
+  useEffect(()=>{
+    
+      if(capital){
+      console.log('capital')
+       axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}&units=metric`)
+     .then(res=>{
+      console.log(res.data)
+      setWeather(res.data)})
+      }
+  },[capital])
+
+  const handleSearch=(e)=>setSearch(e.target.value)
+
+  const handleShow=(name)=>setSearch(name)
+
+  
+    return (
+      <>
+        <div>find Country <input type='text' value={search} onChange={handleSearch}/></div>
+        { (countriesToShow.length<10 && countriesToShow.length>1)
+          ? <SearchList countries={countriesToShow} text="show" handleShow={handleShow}/>
+          :((countriesToShow.length ===1 && Object.keys(weather).length>0 )
+             ? <CountryInfo country={countriesToShow} weather={weather} />
+             :<p>too many matches,please specify.</p>)
+        }
+      </>
   )
 }
 
